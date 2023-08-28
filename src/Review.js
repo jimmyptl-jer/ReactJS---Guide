dd305d2a64b1e739ee21d87369f3d5cd56fec4ea
jimmyptl-jer <jimmypatel687@yahoo.com>
2023-08-27 22:28:40 -0400

    Adding Branch Reviews

---
+++ src/Review.js
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+import people from './data';
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+
+const Review = () => {
+  const [index,setIndex] = useState(0);
+  const {name,job,image,text} = people[index];
+
+  const checkNumber = (number) =>{
+    if (number > people.length -1){
+      return 0;
+    }
+    if ( number < 0){
+      return people.length -1;
+    }
+    return number
+  }
+
+  const NextPerson = () =>{
+    setIndex((index) =>{
+      const newIndex = index +1;
+      return checkNumber(newIndex);
+    })
+  }
+
+  const PrevPerson = () =>{
+    setIndex((index) =>{
+      const newIndex = index -1;
+      return checkNumber(newIndex);
+    })
+  }
+ 
+  const generateRandomNumber = () => {
+    
+    const random = Math.floor(Math.random() * people.length);
+    return random;
+  };
+
+  const RandomPerson = () =>{
+    const random = generateRandomNumber();
+    console.log(random);
+    setIndex(()=>{
+      const newIndex = random;
+      return checkNumber(newIndex);
+    })
+  }
+
+
+  return(
+    <article className='review'>
+      <div className='img-container'>
+        <img src={image} alt={name} className='person-img'/>
+        <span>
+          <FaQuoteRight/>
+        </span>
+      </div>
+      <h4 className='author'>{name}</h4>
+      <p className='job'>{job}</p>
+      <p className='info'>{text}</p>
+      <div className='.button-container'>
+        <button className='next-btn' onClick={NextPerson}>
+          <FaChevronLeft/>
+        </button>
+        <button className='prev-btn' onClick={PrevPerson}>
+          <FaChevronRight/>
+        </button>
+      </div>
+      <button className='random-btn' onClick={RandomPerson}>Surprise Me</button>
+    </article>
+  ) ;
+};
+
+export default Review;
